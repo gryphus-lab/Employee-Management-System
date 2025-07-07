6e0bb317c4450c73a2ac60aa49cf7b7d249507ec
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2025-07-07 19:58:30 +0200

    fix build

--- FrontEnd/src/components/pages/Settings.js
+++ FrontEnd/src/components/pages/Settings.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import PageHeader from '../layout/PageHeader';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
